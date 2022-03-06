cba55dfab1dd485e30a6f8be7ca15dc79f55ca6d
chenchao <chenchao@chenchaos-MBP.lan>
2022-03-06 16:28:02 +0800

    提交一下

---
+++ main.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# coding=utf-8
+#
+# Personal code. All Rights Reserved
+#
+"""
+DESCRIPTION.
+
+Authors: He Zhiqin
+"""
+
+
+def hello_world(name: str):
+    """
+    Args:
+        name:
+
+    Returns:
+
+    """
+    print(f"Hello {name} !")
+
+
+if __name__ == "__main__":
+    hello_world("He_qin")
